135965b73b282fcd478b97c031d775db3575fc8f
NizWasTaken <70901221+NizWasTaken@users.noreply.github.com>
2020-09-20 22:53:15 +0200

    Update bot1.py

--- bot1.py
+++ bot1.py
@@ -4,10 +4,10 @@
 import tweepy
 import time
 
-CONSUMER_KEY = 'marlon la pute'
-CONSUMER_SECRET = 'marlon la pute'
-ACCESS_KEY = 'marlon la pute'
-ACCESS_SECRET = 'marlon la pute'
+CONSUMER_KEY = 'xxxx'
+CONSUMER_SECRET = 'xxxx'
+ACCESS_KEY = 'xxxx'
+ACCESS_SECRET = 'xxxx'
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)

c111d58c225da12f43ee8a176ad85f6a903b5e01
NizWasTaken <70901221+NizWasTaken@users.noreply.github.com>
2020-09-20 22:39:29 +0200

    v2

---
+++ bot1.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy
+import time
+
+
+CONSUMER_KEY = 'IKZ7qH0GR3Z77assB88M7xFwX'
+CONSUMER_SECRET = '5XcUcmceAC9rEHjAGaPsmeIzIGYUmSEJO5JlvHDSvfDx8J20sO'
+ACCESS_KEY = '1307636786311630848-gJgal7HJLLt36cJbPLOfNhnQcTEy6d'
+ACCESS_SECRET = 'eZ7YlTBgoJZ9suGQV3eveQziJpcAcHruhtZYXejfQLlTy'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def reply_to_tweets():
+    print('retrieving and replying to tweets...')
+
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(
+                        last_seen_id,
+                        tweet_mode='extended')
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.full_text)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        if 'ici' in mention.full_text.lower():
+            print('found cc')
+            print('responding back...')
+            api.update_status('oui @' + mention.user.screen_name +
+                    ', il faut fermer les ecoles !', mention.id)
+
+while True:
+    reply_to_tweets()
+    time.sleep(20)

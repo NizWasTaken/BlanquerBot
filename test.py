81de697648f7cd4d39798d50231e18749f1af3a1
NizWasTaken <70901221+NizWasTaken@users.noreply.github.com>
2020-09-21 22:49:52 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy
+import time
+
+CONSUMER_KEY = 'xxxx'
+CONSUMER_SECRET = 'xxxx'
+ACCESS_KEY = 'xxxx-xxxx'
+ACCESS_SECRET = 'xxxx'
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+media = api.media_upload("grenouille.jpg")
+
+FILE_NAME = 'last_seen_id.txt'
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+def reply_to_tweets():
+    print('retrieving tweets...')
+
+    last_seen_id = retrieve_last_seen_id(FILE_NAME)
+    mentions = api.mentions_timeline(
+                        last_seen_id,
+                        tweet_mode='extended')
+    for mention in reversed(mentions):
+        print(str(mention.id) + ' - ' + mention.full_text)
+        last_seen_id = mention.id
+        store_last_seen_id(last_seen_id, FILE_NAME)
+        
+        if 'grenouille' in mention.full_text.lower():
+            print('found mention')
+            print('responding back with image...')
+            api.update_with_media('grenouille.jpg', in_reply_to_status_id=mention.id)
+
+while True:
+    reply_to_tweets()
+    time.sleep(15)
+
+
+
+
+
+
+   
